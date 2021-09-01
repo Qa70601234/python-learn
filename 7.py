d2fb53b4e23b4877a59758581fc001ae6c0f43e2
Qa70601234 <2798360606@qq.com>
2021-09-01 11:13:42 +0800

    7

--- 7.py
+++ 7.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
-import os
+from time import time
+
+
+
+
+def main():
+    total = 0
+    num = [x for x in range(1,100000001)]
+    start = time()
+    for  i in num:
+        total += i 
+    print(total)
+    end = time()
+    print('耗时：%.3fs' % (end - start))
+if __name__ == '__main__':
+    main()
